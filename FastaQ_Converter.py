337fc811866ea60d15255c4e6dc2694e72d54d66
Yancheng Liu <morningsherry@hotmail.com>
2013-10-27 17:55:16 -0400

    Update FastaQ_Converter.py

--- FastaQ_Converter.py
+++ FastaQ_Converter.py
@@ -5,11 +5,4 @@ After conversion, the raw data are ready for analysis by tools provided on NCBI 
 '''
 from Bio import SeqIO
 
-records = (rec[:21] for rec in SeqIO.parse(open("untrimmed.fq"), "fastq-solexa"))
-
-savedFile = open("trimmed21.fastq", "w")
-
-count = SeqIO.write(records, savedFile, "fastq-solexa")
-
-handle.close()
-
+SeqIO.convert('6427_6418_N_in_R1.fastq','fastq','trimmed_in.fasta','fasta')
