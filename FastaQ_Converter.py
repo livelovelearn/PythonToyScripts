b43427cb8af920fe775497090586a8d1bf24fb4d
Yancheng Liu <morningsherry@hotmail.com>
2013-10-27 17:59:27 -0400

    Update FastaQ_Converter.py

--- FastaQ_Converter.py
+++ FastaQ_Converter.py
@@ -1,6 +1,6 @@
 '''
 This program help to convert DNA sequences obtained from next generation sequencing.
-After conversion, the raw data are ready for analysis by tools provided on NCBI platform.
+After conversion, the raw data are ready for analysis by tools provided on NCBI Genome Workbench.
 
 '''
 from Bio import SeqIO
