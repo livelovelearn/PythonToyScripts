aca6d50d7bbe2903ce2d22357be11482cab0c9a5
Yancheng Liu <morningsherry@hotmail.com>
2013-10-27 17:57:50 -0400

    Update FastaQ_Converter.py

--- FastaQ_Converter.py
+++ FastaQ_Converter.py
@@ -1,8 +1,8 @@
 '''
-This program help to convert and trim DNA sequences obtained from next generation sequencing.
+This program help to convert DNA sequences obtained from next generation sequencing.
 After conversion, the raw data are ready for analysis by tools provided on NCBI platform.
 
 '''
 from Bio import SeqIO
 
-SeqIO.convert('6427_6418_N_in_R1.fastq','fastq','trimmed_in.fasta','fasta')
+SeqIO.convert('input.fastq','fastq','output.fasta','fasta')
